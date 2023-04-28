3d945530b1385b2828a801c58b8d31c8e1e96ec3
aalnoman10 <aalnomanm10@gmail.com>
2023-04-28 23:13:45 +0600

    Update categories && news

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const port = process.env.PORT || 5000;
 const cors = require('cors');
 
 const cate = require('./data/cate.json');
+const news = require('./data/News.json');
 
 app.use(cors())
 app.get('/', (req, res) => {
@@ -12,6 +13,27 @@ app.get('/', (req, res) => {
 
 app.get('/cate', (req, res) => {
     res.send(cate)
+})
+
+app.get('/cate/:id', (req, res) => {
+    const id = req.params.id
+    if (id === '0') {
+        res.send(news)
+    }
+    else {
+        const selected = news.filter(c => c.category_id === id)
+        res.send(selected)
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id
+    const selected = news.find(n => n._id === id)
+    res.send(selected)
 })
 
 app.listen(port, () => {
